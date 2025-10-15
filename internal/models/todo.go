c0a04343d6d463fa39900a9e50fee46e4b27cce8
Aldiyar <aldikzhaks@gmail.com>
2025-10-16 00:18:16 +0500

    add(): todo and user model

--- internal/models/todo.go
+++ internal/models/todo.go
@@ -1,16 +1,27 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Todo struct {
-	ID          int64     `json:"id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description,omitempty"`
-	Completed   bool      `json:"completed"`
-	CreatedAt   time.Time `json:"created_at"`
+	ID          int64          `json:"id" gorm:"primaryKey;autoIncrement"`
+	Title       string         `json:"title" gorm:"type:varchar(255);not null"`
+	Description string         `json:"description,omitempty" gorm:"type:text"`
+	Completed   bool           `json:"completed" gorm:"default:false"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `json:"-" gorm:"index"`
+
+	UserID int64 `json:"user_id" gorm:"not null;index"`
+	User   User  `json:"user,omitempty" gorm:"foreignKey:UserID"`
 }
+
 type TodoRequest struct {
-	Title       string `json:"title"`
+	Title       string `json:"title" validate:"required"`
 	Description string `json:"description,omitempty"`
 	Completed   bool   `json:"completed"`
+	UserID      int64  `json:"user_id,omitempty"`
 }
---
+++ internal/models/user.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID           int64          `json:"id" gorm:"primaryKey;autoIncrement"`
+	Name         string         `json:"name" gorm:"type:varchar(255);not null"`
+	Username     string         `json:"username" gorm:"type:varchar(255);uniqueIndex;not null"`
+	PasswordHash string         `json:"-" gorm:"type:varchar(255);not null"`
+	CreatedAt    time.Time      `json:"created_at"`
+	UpdatedAt    time.Time      `json:"updated_at"`
+	DeletedAt    gorm.DeletedAt `json:"-" gorm:"index"`
+
+	Todos []Todo `json:"todos,omitempty" gorm:"foreignKey:UserID"`
+}
+
+type CreateUserRequest struct {
+	Name     string `json:"name" validate:"required"`
+	Username string `json:"username" validate:"required"`
+	Password string `json:"password" validate:"required,min=6"`
+}
+
+type UserResponse struct {
+	ID       int64  `json:"id"`
+	Name     string `json:"name"`
+	Username string `json:"username"`
+}
