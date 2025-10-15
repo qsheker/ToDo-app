468cf0d7a946c4eb164a91b7b85abca25e7be0da
Aldiyar <aldikzhaks@gmail.com>
2025-10-16 00:08:31 +0500

    feat(): database with GORM config

--- internal/repository/database.go
+++ internal/repository/database.go
@@ -1,1 +1,37 @@
 package repository
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+func NewDB(cfg Config) (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		cfg.Host, cfg.Username, cfg.Password, cfg.DBName, cfg.Port, cfg.SSLMode,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Connected to PostgreSQL with GORM")
+	return db, nil
+}
+
+func AutoMigrate(db *gorm.DB) error {
+	return db.AutoMigrate()
+}
